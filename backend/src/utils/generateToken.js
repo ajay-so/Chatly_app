84c93814f15a6aa0138dacbe2ebd3f075b219f55
Ajay Kumar <ajaykumar89693479@gmail.com>
2026-01-07 12:48:48 +0530

    Fix the cookie related issue and update the self url

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -10,8 +10,8 @@ const createToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: ENV.NODE_ENV === 'production' ? true : false
+        sameSite: "none",
+        secure: true
     });
 
     return token;
