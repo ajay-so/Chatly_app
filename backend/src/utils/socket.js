2fafa92c4fae589ffd63bf3349811f731eb75e57
Ajay Kumar <ajaykumar89693479@gmail.com>
2026-01-07 21:10:12 +0530

    added the deployed frontend url

--- backend/src/utils/socket.js
+++ backend/src/utils/socket.js
@@ -2,13 +2,14 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const { socketAuthMiddleware } = require("../middlewares/socket.auth.middleware.js");
+const { ENV } = require("./env.js");
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ENV.FRONTEND_URL,
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     },
